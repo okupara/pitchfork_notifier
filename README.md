890b2f7be5980840166f802e5196b889dc836647
okupara <okupara@okupara-no-MacBook-Pro.local>
2016-12-06 02:33:38 +0900

    added some docs.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # pitchfork_notifier
 This program notifies information of albums in Pitchfork to a slack channel when the program find out albums which these scores are higher than you set.
 
+## requires
+It is worked by Lamda Function, Dynamo DB, API Gateway and Slack webhooks.
 
 ![image](./img/posted_image.png)
--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 const app = require('./lib/app');
 
+/**
+ * This function is an entry point of this program.
+ * It is called by API Gateway and Scheduled Trigger in Lambda Function.
+ * 
+ * @param {Object} event The information of an invoking environment.
+ * @param {Object} context
+ * @param {Function} callback
+ */
+
 exports.handler = function (event, context, callback) {
   if (!event || typeof(event) !== 'object' || Object.keys(event) === 0) {
     return callback('Please specify a command to run as event');
